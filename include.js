fea04711edb786cdd95fdbe795f83df8a32bbd8c
Gee Brain <gozkybrain@gmail.com>
2025-04-22 21:07:32 +0100

    minor update

--- include.js
+++ include.js
@@ -1,10 +1,9 @@
-
-
 //  ! This will be used to include the page content.html
 fetch("./content.html")
   .then(response => {
     return response.text()
   })
   .then(data => {
+    // geebrain will be used ase <geebrain />
     document.querySelector("geebrain").innerHTML = data;
   });
